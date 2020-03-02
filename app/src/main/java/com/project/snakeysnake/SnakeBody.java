24e23b146262167152013cfc363b15831163d177
cfoulk <charlesfoulk@csus.edu>
2020-03-01 21:59:09 -0800

    composite design pattern structure

---
+++ app/src/main/java/com/project/snakeysnake/SnakeBody.java
@@ -0,0 +1,22 @@
+package com.project.snakeysnake;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import java.util.ArrayList;
+
+public class SnakeBody {
+
+    private Bitmap mBitmapBody;
+
+    public void draw(ArrayList<Point> segmentLocations, int mSegmentSize, Paint paint){
+        for (int i = 1; i < segmentLocations.size(); i++) {
+            canvas.drawBitmap(mBitmapBody,
+                    segmentLocations.get(i).x
+                            * mSegmentSize,
+                    segmentLocations.get(i).y
+                            * mSegmentSize, paint);
+    }
+}
---
+++ app/src/main/java/com/project/snakeysnake/SnakeComponent.java
@@ -0,0 +1,9 @@
+package com.project.snakeysnake;
+
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import java.util.ArrayList;
+
+public interface SnakeComponent {
+}
