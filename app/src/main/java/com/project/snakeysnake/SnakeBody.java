42a1115c5e0c6c3d9c7427454f04b4a48349d476
cfoulk <charlesfoulk@csus.edu>
2020-03-02 14:46:11 -0800

    could be bad code

--- app/src/main/java/com/project/snakeysnake/SnakeBody.java
+++ app/src/main/java/com/project/snakeysnake/SnakeBody.java
@@ -42,11 +42,12 @@ public class SnakeBody implements SnakeComponent{
         }
     }
 
+    /* might be bad code
     public void remove(ArrayList<Point> segmentLocations, Canvas canvas, Paint paint){
         if (segmentLocations.size() > 0){
             for (int i = 1; i < 2; i++){
                 segmentLocations.remove(segmentLocations.size()-1);
             }
         }
-    }
+    } */
 }
